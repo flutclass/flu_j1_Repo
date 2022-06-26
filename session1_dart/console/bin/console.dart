307b3f725f08ef4122b32a10e50a3f6a6f526735
flutclass <fluttclassgr1@gmail.com>
2022-06-26 23:11:05 +0430

    first_session

---
+++ session1_dart/console/bin/console.dart
@@ -0,0 +1,186 @@
+import 'package:console/console.dart' as console;
+
+void main() {
+// var / final / const 
+// var num =12;
+// var str = "asd";
+// var ashari = 3.6;
+//-----------------
+// int  num =23;
+// String Str = "sada";
+// double ashari = 3.5;
+// bool op = true;
+// const oz = true;
+///-----------------------
+// const k = 12;
+    // k = 32; // Error
+// final l = 32;
+  // l = 98;
+// print(lstK);
+///-------------------------
+// String k = "abtin";
+// // String Name = 'abtin';
+// String socialMedia = 'what s\' ${num} app';
+// String names = '''aasdassdasad
+// asddsasadsad
+// asdadssadsad
+// dkkkd
+// asddassdasad
+// kkkkkk
+// kdkk
+// sasadsadads
+// asdsadasddsa
+// sadasdd
+// ''';
+
+///--------------
+// var lstSet = {'ali',123,'abtin',true,'akbar'};
+// print(lstSet);
+
+// var Alphabets = ['a','b','c','d'];
+// print(lstSet);  
+
+// var Person = {'name':'abtin',
+//   'age':23,
+//  'IsMarried' :true};
+
+// Map<String,dynamic> Person = 
+// {'age':'23',
+// 'Id_num':2332,
+// 'FName':"asghar",'LName':'akbari'};
+// print(Person['FName']);
+//----------------------
+// + - / * % ~/ 
+
+// print(a+b);
+// print(a-b);
+// print(a/b);
+// print(a*b);
+// print(a%b);
+// print(a~/b);
+//-------------------------
+// print(a == b);
+// print(a > b);
+// print(a<b);
+// print(a!=b);
+// print(a <= b);
+// print(a >=b);
+//-------------------------------
+// print( a is! int);
+// print(a is String);
+// print( a as String);
+//-----------------------------------
+// int i =21;
+// a -=b;
+// a /= b;
+// print(i++);
+// print(++i);
+// print(i--);
+// print(--i);
+
+// a = 98;
+// b = 73;
+////--------
+// //a + b => a
+// // a++
+// int a = 29;
+// int b =20;
+// //    False    True
+// print(a==b && a >= b);
+// //     True      True
+// print(!(a==b) && a > b);
+
+// String Role = 'Normal';
+
+// int age = 20 ;
+// String Result = (Role == 'admin') ?"you are an admin" : "you are not an admin";
+// print(Result);
+
+
+// if (role=="admin") {
+//   print("hello admin");
+// }
+// else if (role=="normal"){
+//   print("hello normal user");
+// }
+// else if(role=="vip"){
+//   print("hello vip user");
+// }
+// else {
+//    print('unauthorized');
+// }
+        // String role = "admin";
+        // switch (role) {
+        //   case "admin":
+        //     print("hello admin");
+        //     break;
+        //   case "normal":
+        //     print("hello normal user");
+        //     break; 
+        //     case "vip":
+        //     print("hello vip user");
+        //     break;   
+        //   default:
+        //   print('unauthorized');
+        //   break;
+        // }
+    //  for (var i = 1; i < 101; i++) {
+    //       print(i);
+      
+    //  }   
+        // var numbers = [1,3,5,6,10,23,13,22,20,897,27];
+        // List<int> lstTemp = [];
+        // lstTemp.add(23);
+        // for (var element in numbers) {
+        //   print(element);
+        // }
+// int i = 5;
+// while (i<10) {
+//   print('hello world');
+//   i++;
+  
+// }
+
+// do {
+//     print('hello world');
+
+// } while (i <=5 );
+
+
+          // print(1);
+          // print(2);
+          // print(3);
+
+
+// int a = 89;
+// int b = 27;
+// int c = 78;
+
+// print( a + b);
+// print(a * b);
+// print(a + b + c) ;
+
+
+// void Add(int a , int b , int c){
+
+// print( a + b + c);
+
+// }
+// int Add (int a , int b){
+
+// return a+b;
+
+// }
+//  int a = 23;
+//  int b = 87;
+// int result = Add(a = 32,b= 23);
+// print(result);
+// int x = 5;
+// do {
+//   print('Hello world');
+// } while (x!=5);
+
+
+
+ 
+}
