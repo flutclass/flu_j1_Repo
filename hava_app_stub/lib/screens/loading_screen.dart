8bc40f4a175a3e10e3ffb5c6836419d5fa9a0541
flutclass <fluttclassgr1@gmail.com>
2022-08-13 21:53:34 +0430

    hava app

--- hava_app_stub/lib/screens/loading_screen.dart
+++ hava_app_stub/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:convert';
+import 'package:http/http.dart'as http;
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,27 +10,45 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Position? _position;
 
-  void _getCurrentLocation() async {
-    Position position = await _determinePosition();
-    setState(() {
-      _position = position;
-    });
+  Position? _postion;
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+    getData();
   }
+  void getData() async{
+    http.Response response =
+    await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=London&appid=7863fcfa97fb15bab2ed87005549acf4'));
+    // print(response.body);
+    if(response.statusCode ==200){
+      String data = response.body;
+      var WeatherDescription = jsonDecode(data)['weather'][0]['description'];
+      print(WeatherDescription);
 
-  Future<Position> _determinePosition() async {
-    LocationPermission permission;
-
-    permission = await Geolocator.checkPermission();
-
-    if(permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if(permission == LocationPermission.denied) {
-        return Future.error('Location Permissions are denied');
-      }
+    }
+    else{
+      print(response.statusCode);
     }
 
+  }
+  void _getCurrentLocation() async{
+    Position postion = await _determinPostion();
+    setState(() {
+      _postion = postion;
+    });
+    print(_postion);
+  }
+  Future<Position> _determinPostion() async{
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+      if(permission == LocationPermission.denied){
+        return Future.error(" Location permistions are denied");
+      }
+    }
     return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
   }
 
@@ -36,16 +57,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    String myMargin = 'abc';
+    double myMarginAsDouble =30.0;
+    try{
+       myMarginAsDouble = double.parse(myMargin);
+
+    }
+    catch(ex){
+      myMarginAsDouble = 50;
+
+    }
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed:  _getCurrentLocation,
-
-            //Get the current location
-
-          child: Text('Get Location'),
-        ),
+      body: Container(
+        margin: EdgeInsets.all(myMarginAsDouble),
+        color: Colors.red,
       ),
     );
+
   }
 }
